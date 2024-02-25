2a96f3ed3b103e56dc5df9d46faa31ae20d347ee
posada <dapiposada@>
2024-02-25 14:41:30 -0600

    before react-icons

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+
+
 export default function Page() {
-  return <h1>Page</h1>;
+
+  return (
+    <header className="headerContainer">
+       <div className="imgContainer">
+            <Link href={"/"}><img alt="" src="/img/logoB.png"></img></Link>
+        </div>
+        <form className="search">
+            <label htmlFor="searchbar"><input id="searchbar" type="text" /></label>
+        </form>
+        <div className="titleContainer">
+          <Link href={'../books'} ><button type="submit" className="btn btn-primary">Search</button></Link>
+        </div>
+    </header>
+  );
 }
